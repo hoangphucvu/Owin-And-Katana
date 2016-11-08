908871260ffaf91c4cad0a3028555ab3cc24b9dc
hoangphucvu <ngohungphuc95@gmail.com>
2016-11-08 15:08:20 +0700

    Write request time

--- Owin-And-Katana/Owin-And-Katana/Middleware/DebugMiddleware.cs
+++ Owin-And-Katana/Owin-And-Katana/Middleware/DebugMiddleware.cs
@@ -13,20 +13,29 @@ namespace Owin_And_Katana.Middleware
 {
     public class DebugMiddleware
     {
-        private AppFunc _next;
+        private readonly AppFunc _next;
+        private DebugMiddlewareOptions _options;
 
-        public DebugMiddleware(AppFunc next)
+        public DebugMiddleware(AppFunc next, DebugMiddlewareOptions options)
         {
             _next = next;
+            _options = options;
+            if (_options.OnIncomingRequest == null)
+            {
+                _options.OnIncomingRequest = (ctx) => { Debug.WriteLine("Incoming request: " + ctx.Request.Path); };
+            }
+            if (_options.OnOutgoingRequest == null)
+            {
+                _options.OnOutgoingRequest = (ctx) => { Debug.WriteLine("Outgoing request: " + ctx.Request.Path); };
+            }
         }
 
         public async Task Invoke(IDictionary<string, object> enviroment)
         {
             var ctx = new OwinContext(enviroment);
-            var path = (string)enviroment["owin.RequestPath"];
-            Debug.WriteLine("Incoming request: " + ctx.Request.Path);
+            _options.OnIncomingRequest(ctx);
             await _next(enviroment);
-            Debug.WriteLine("Outgoing request: " + ctx.Request.Path);
+            _options.OnOutgoingRequest(ctx);
         }
     }
 }
--- Owin-And-Katana/Owin-And-Katana/Startup.cs
+++ Owin-And-Katana/Owin-And-Katana/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Owin;
 using Owin;
 using Owin_And_Katana.Middleware;
+using System.Diagnostics;
 
 [assembly: OwinStartup(typeof(Owin_And_Katana.Startup))]
 
@@ -11,7 +12,21 @@ namespace Owin_And_Katana
         public void Configuration(IAppBuilder app)
         {
             // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
-            app.Use<DebugMiddleware>();
+            app.Use<DebugMiddleware>(new DebugMiddlewareOptions
+            {
+                OnIncomingRequest = (ctx) =>
+                {
+                    var watch = new Stopwatch();
+                    watch.Start();
+                    ctx.Environment["DebugStopwatch"] = watch;
+                },
+                OnOutgoingRequest = (ctx) =>
+               {
+                   var watch = (Stopwatch)ctx.Environment["DebugStopwatch"];
+                   watch.Stop();
+                   Debug.WriteLine("Request took " + watch.ElapsedMilliseconds + " ms");
+               }
+            });
             app.Use(async (ctx, next) =>
             {
                 await ctx.Response.WriteAsync("<html><head></head><body>Hello World</body></html>");
