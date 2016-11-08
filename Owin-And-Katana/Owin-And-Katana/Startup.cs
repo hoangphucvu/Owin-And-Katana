b51cae393ee7bf3686c2fff9afc43f0c8cea6d8f
hoangphucvu <ngohungphuc95@gmail.com>
2016-11-08 10:16:28 +0700

    Setup owin

---
+++ Owin-And-Katana/Owin-And-Katana/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Owin_And_Katana.Startup))]
+
+namespace Owin_And_Katana
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            app.Use(async (ctx, next) =>
+            {
+                await ctx.Response.WriteAsync("<html><head></head><body>Hello World</body></html>");
+            });
+        }
+    }
+}
